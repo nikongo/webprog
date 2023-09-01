96e32d8bff77c38eefbe3454c1a93dca15445dc5
André Philipsson <andre@philipsson.me>
2023-09-01 13:03:30 +0200

    More updates

--- labs/lab2/src/ComposeSalad.js
+++ labs/lab2/src/ComposeSalad.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Salad from './Salad';
 
 function SaladSelect({ value, onChange, values }) {
   return (
@@ -20,14 +21,26 @@ function ComposeSalad(props) {
   const extras = inventory.filter(name => props.inventory[name].extra);
 
   const [foundation, setFoundation] = useState('Pasta');
-  const [protein, setProtein] = useState();
-  const [dressing, setDressing] = useState();
+  const [protein, setProtein] = useState('Kycklingfilé');
+  const [dressing, setDressing] = useState('Ceasardressing');
   const [extra, setExtra] = useState({ Bacon: true, Fetaost: true });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    props.addSalad(new Salad({ foundation, protein, dressing, ...extra }));
+
+    setFoundation('Pasta');
+    setProtein('Kycklingfilé');
+    setDressing('Ceasardressing');
+    setExtra({ Bacon: true, Fetaost: true });
+  }
 
   return (
     <div className="container col-12">
       <div className="row h-200 p-5 bg-light border rounded-3">
-        <form>
+        <h2>Välj innehållet i din sallad</h2>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label className="form-label">Välj bas</label>
             <SaladSelect value={foundation} onChange={setFoundation} values={foundations} />
@@ -38,15 +51,10 @@ function ComposeSalad(props) {
             <SaladSelect value={protein} onChange={setProtein} values={proteins} />
           </div>
 
-          <div className="mb-3">
-            <label className="form-label">Välj dressing</label>
-            <SaladSelect value={dressing} onChange={setDressing} values={dressings} />
-          </div>
-
-          <div className="mb-3">
-            <label className="form-label">Välj extra</label>
+          <span className="form-label">Välj tillbehör</span>
+          <div className="mb-3 d-flex flex-wrap justify-content-start">
             {extras.map(name => (
-              <div key={name} className="form-check">
+              <div key={name} className="form-check" style={{ flexBasis: "25%" }}>
                 <input
                   className="form-check-input"
                   type="checkbox"
@@ -58,6 +66,13 @@ function ComposeSalad(props) {
               </div>
             ))}
           </div>
+
+          <div className="mb-3">
+            <label className="form-label">Välj dressing</label>
+            <SaladSelect value={dressing} onChange={setDressing} values={dressings} />
+          </div>
+
+          <button className="btn btn-primary" type="submit">Beställ</button>
         </form>
       </div>
     </div>
