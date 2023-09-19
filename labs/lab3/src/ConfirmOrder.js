061be8924c121e8c10f47f3f7b12b90aab7047ff
André Philipsson <andre@philipsson.me>
2023-09-19 15:50:46 +0200

    Update ConfirmOrder.js

--- labs/lab3/src/ConfirmOrder.js
+++ labs/lab3/src/ConfirmOrder.js
@@ -5,16 +5,14 @@ export default function ConfirmOrder() {
   const props = useOutletContext();
 
   const salad = props.cart.find(salad => salad.id === id);
-  return (salad) ?
+  return (!salad) ?
     (
       <div className="mt-3 alert alert-danger alert-dismissible fade show" role="alert">
         <p>Vi hittade inte din beställning</p>
-        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Stäng"></button>
       </div>
     ) : (
       <div className="mt-3 alert alert-success alert-dismissible fade show" role="alert">
         <p>Din beställning är bekräftad!</p>
-        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Stäng"></button>
       </div>
     )
 }
